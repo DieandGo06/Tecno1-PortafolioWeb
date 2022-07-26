885568d6710a1e06b554a2a2f349ed7b828c1135
DieandGo06 <66442220+DieandGo06@users.noreply.github.com>
2022-07-26 11:00:36 -0300

    Add files via upload

---
+++ js/inicia.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+});
+
+$(document).ready(function(){
+ $('.parallax').parallax();
+});
+
+$(document).ready(function(){
+  $('.scrollspy').scrollSpy();
+});
